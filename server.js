12ba00eb1ccb6698bf8d7ad0a67ef1988e3b8693
unknown <the6861@naver.com>
2025-03-05 17:16:48 +0900

    project 등록, 조회, 이미지 저장 추가

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const app = express();
 const models = require("./models");
 const port = 8080;
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/boards", (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 1;
@@ -33,6 +45,23 @@ app.get("/boards", (req, res) => {
     });
 });
 
+app.get("/projects", (req, res) => {
+  models.Project.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["name", "imageUrl", "hubUrl"],
+  })
+    .then((result) => {
+      console.log("PROJECT : ", result);
+      res.send({
+        projects: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("프로젝트 불러오기 에러 발생");
+    });
+});
+
 app.post("/insertBoard", (req, res) => {
   const body = req.body;
   const { title, contents } = body;
@@ -53,6 +82,37 @@ app.post("/insertBoard", (req, res) => {
       console.error(error);
       res.status(400).send("게시글 등록에 문제가 발생했습니다.");
     });
+});
+
+app.post("/insertProject", (req, res) => {
+  const body = req.body;
+  const { name, imageUrl, hubUrl } = body;
+  if (!name || !imageUrl || !hubUrl) {
+    res.status(400).send("모든 필드를 입력하세요.");
+  }
+  models.Project.create({
+    name,
+    imageUrl,
+    hubUrl,
+  })
+    .then((result) => {
+      console.log("프로젝트 등록 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("프로젝트 등록에 문제가 발생했습니다.");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 app.listen(port, () => {
